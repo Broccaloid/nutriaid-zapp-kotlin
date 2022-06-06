4c522b1599ff0605dabaaadbcd554d1442893cbe
Tymofii Tarasenko <tarasenko.t.2001@gmail.com>
2022-06-06 21:12:36 +0200

    Added macros complex search parameters

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/models/requests/FullRecipeParameters.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/models/requests/FullRecipeParameters.kt
@@ -2,9 +2,12 @@ package com.example.nutriaid_zapp_kotlin.models.requests
 
 import com.example.nutriaid_zapp_kotlin.models.interfaces.IQueryParameters
 
-class FullRecipeParameters (val id: Int, private val includeNutrition: Boolean = true) : IQueryParameters {
+class FullRecipeParameters(
+    val id: Int,
+    private val includeNutrition: Boolean = true
+) : IQueryParameters {
     override fun getQuerySearchParameters(): Map<String, String> {
-        var parameters = HashMap<String, String>()
+        val parameters = HashMap<String, String>()
         parameters["includeNutrition"] = includeNutrition.toString()
         parameters["apiKey"] = "20191a90b51c46c3bb1ae5945c400586"
         return parameters
--- app/src/main/java/com/example/nutriaid_zapp_kotlin/models/requests/SearchParameters.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/models/requests/SearchParameters.kt
@@ -7,7 +7,15 @@ class SearchParameters(
     private val cuisine: String? = null,
     private val diet: String? = null,
     private val query: String? = null,
-    private val intolerances: String? = null
+    private val intolerances: String? = null,
+    private val minCarbs: String? = null,
+    private val minProtein: String? = null,
+    private val minFat: String? = null,
+    private val minCalories: String? = null,
+    private val maxCarbs: String? = null,
+    private val maxProtein: String? = null,
+    private val maxFat: String? = null,
+    private val maxCalories: String? = null,
 ) : IQueryParameters {
     override fun getQuerySearchParameters() : Map<String, String>{
         val parameters = HashMap<String, String>()
@@ -16,6 +24,14 @@ class SearchParameters(
         diet?.let { parameters.put("diet", it) }
         query?.let { parameters.put("name", it) }
         intolerances?.let { parameters.put("intolerances", it) }
+        minCarbs?.let { parameters.put("minCarbs", it) }
+        minProtein?.let { parameters.put("minProtein", it) }
+        minFat?.let { parameters.put("minFat", it) }
+        minCalories?.let { parameters.put("minCalories", it) }
+        maxCarbs?.let { parameters.put("maxCarbs", it) }
+        maxProtein?.let { parameters.put("maxProtein", it) }
+        maxFat?.let { parameters.put("maxFat", it) }
+        maxCalories?.let { parameters.put("maxCalories", it) }
         parameters["apiKey"] = "20191a90b51c46c3bb1ae5945c400586"
         return parameters
     }
