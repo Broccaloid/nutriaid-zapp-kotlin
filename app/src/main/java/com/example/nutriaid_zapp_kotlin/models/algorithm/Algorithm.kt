bc95b664da2945ac3461b7f82e12ed4bac2ad612
JannisMeyer <80952891+JannisMeyer@users.noreply.github.com>
2022-06-13 19:38:47 +0200

    algorithm rewrote

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/models/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/models/algorithm/Algorithm.kt
@@ -1,25 +1,37 @@
 package com.example.nutriaid_zapp_kotlin.models.algorithm
 
 import com.example.nutriaid_zapp_kotlin.models.requests.SearchParameters
-
-lateinit var search: SearchParameters
+import com.example.nutriaid_zapp_kotlin.api_services.SpoonacularService
+import com.example.nutriaid_zapp_kotlin.models.short_recipe.RecipeShortData
+import com.example.nutriaid_zapp_kotlin.repositories.ApiRepository
 
 fun algorithm(userSpecs: userSpecs){
-    search.number = 3
-    search.diet = userSpecs.diet
-    search.intolerances = userSpecs.intolerances
+    var number: String = "3"
+    var cuisine: String //for recipe-exploring
+    var diet: String //for recipe-exploring
+    var minCarbs: String = "0"
+    var minProtein: String = "0"
+    var minCalories: String = "0"
+    var maxFat: String = "0"
+    var maxCalories: String = "0"
 
     for (i in userSpecs.dietExtras){
         if(i == "high-protein")
-            search.minProtein = "30"
+            minProtein = "30"
         if(i == "low-calorie")
-            search.maxCalories = "500"
+            maxCalories = "500"
         if(i == "high-calorie")
-            search.minCalories = "700"
+            minCalories = "700"
         if(i == "high-carb")
-            search.minCarbs = "100"
+            minCarbs = "100"
         if(i == "low-fat")
-            search.maxFat = "20"
+            maxFat = "20"
     }
 
+    var search = SearchParameters(number, null, userSpecs.diet, null, userSpecs.intolerances, minCarbs, minProtein, null, minCalories, null, null, maxFat, maxCalories)
+    var return : RecipeShortData = SpoonacularService.getListShortRecipes(search)
+
+    //TODO("write return to firebase")
+    //TODO("get return to view-models")
+    //TODO("create alarm, whose intent launches the algorithm")
 }
