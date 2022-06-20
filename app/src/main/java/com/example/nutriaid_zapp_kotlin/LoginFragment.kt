42bccd37492eb00204c5ad764f4b7b26a7f07a36
IanB2000 <ianluka.brekalo@gmail.com>
2022-06-20 21:03:43 +0200

    Added Firebase configurations

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/LoginFragment.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/LoginFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.nutriaid_zapp_kotlin.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
