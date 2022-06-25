55119c8995bd030e17dcc1d8e3bb3bfcc0384661
AnnaSchuerer <anna.schuerer@t-online.de>
2022-06-25 16:21:27 +0200

    added MPAndroidChart dependencies

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/ChartFragment.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/ChartFragment.kt
@@ -1,10 +1,27 @@
 package com.example.nutriaid_zapp_kotlin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nutriaid_zapp_kotlin.databinding.FragmentChartBinding
 
-/**
- * A simple [Fragment] subclass.
- */
-class ChartFragment : Fragment(R.layout.fragment_chart) {
+class ChartFragment : Fragment() {
+    private var _binding: FragmentChartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentChartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
 
 }
