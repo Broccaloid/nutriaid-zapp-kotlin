2259b0a7384c4b9bc89fbf82508a7b047c69b687
JannisMeyer <80952891+JannisMeyer@users.noreply.github.com>
2022-06-25 18:09:25 +0200

    getRecipeIdList renamed to getRecipes, because the return is no longer needed, Alarm.kt rewrote, AlarmReceiver rewrote

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/models/algorithm/AlarmReceiver.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/models/algorithm/AlarmReceiver.kt
@@ -3,7 +3,7 @@ package com.example.nutriaid_zapp_kotlin.models.algorithm
 import android.content.Intent
 import android.content.BroadcastReceiver
 import android.content.Context
-import com.example.nutriaid_zapp_kotlin.models.algorithm.Algorithm
+import com.example.nutriaid_zapp_kotlin.models.algorithm.IAlgorithm
 
 class AlarmReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
