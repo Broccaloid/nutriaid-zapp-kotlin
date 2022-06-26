c0ef45909c0c4f36dd5765534e44d104cb01bc0d
Tymofii Tarasenko <tarasenko.t.2001@gmail.com>
2022-06-27 00:47:01 +0200

    Implemented filters for RecipeListFragment

--- app/src/main/java/com/example/nutriaid_zapp_kotlin/fragments/RecipeListFragment.kt
+++ app/src/main/java/com/example/nutriaid_zapp_kotlin/fragments/RecipeListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -35,6 +36,7 @@ class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentRecipeListBinding.inflate(layoutInflater, container, false)
+
         viewModel = ViewModelProvider(
             this,
             RecipeFragmentViewModelFactory(ApiRepository(spoonacularService))
@@ -47,11 +49,42 @@ class RecipeListFragment : Fragment(R.layout.fragment_recipe_list) {
         viewModel.errorMessage.observe(viewLifecycleOwner, Observer {
         })
         viewModel.getListShortRecipes(SearchParameters())
+
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intolerances = resources.getStringArray(R.array.intolerances)
+        val intolerancesAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, intolerances)
+        binding.intolerance.setAdapter(intolerancesAdapter)
+
+        val cuisines = resources.getStringArray(R.array.cuisines)
+        val cuisinesAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, cuisines)
+        binding.cuisine.setAdapter(cuisinesAdapter)
+
+        val diets = resources.getStringArray(R.array.diets)
+        val dietsAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, diets)
+        binding.diet.setAdapter(dietsAdapter)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.applyFiltersButton.setOnClickListener {
+            var diet: String? = null
+            var cuisine: String? = null
+            var intolerance: String? = null
+            if (binding.diet.text.toString() != "None"){
+                diet = binding.diet.text.toString()
+            }
+            if (binding.cuisine.text.toString() != "None"){
+                cuisine = binding.cuisine.text.toString()
+            }
+            if (binding.intolerance.text.toString() != "None"){
+                intolerance = binding.intolerance.text.toString()
+            }
+            viewModel.getListShortRecipes(SearchParameters(diet = diet, cuisine = cuisine, intolerances = intolerance))
+        }
     }
 
     companion object {
